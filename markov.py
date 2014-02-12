9c6d77dd2d3b4b3ed1ffc802c9502ffc6ea33bec
Hackbright Student <info@hackbrightacademy.com>
2014-02-12 11:52:56 -0800

    Picked a more random key to start

--- markov.py
+++ markov.py
@@ -8,32 +8,30 @@ from random import choice
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
+
     # split the string so we have a list of words
     
     input_words = corpus.split()
-
 
     markov_dict = {}
 
     # iterate through list and create markov dictionary
     #  for each tuple -- current word and next word 
     #  if that tuple exists in the dictionary, append the value with a new word
+
     lw = len(input_words)
     for i in range (lw-2):
         if not markov_dict.get((input_words[i],input_words[i+1]),False):
             markov_dict[(input_words[i],input_words[i+1])] = [input_words[i+2]]
         else:
             markov_dict[(input_words[i],input_words[i+1])].append(input_words[i+2])
-   
-    # for key,value in markov_dict.iteritems():
-    #     print key,value
 
-# We ommited the very last bigram in the text.  We need to add it manually, and assign a value
+    # We ommited the very last bigram in the text.  We need to add it manually, and assign a value
 
     markov_dict[(input_words[lw-2],input_words[lw-1])] = [input_words[0]]
     markov_dict[(input_words[lw-1],input_words[0])] = [input_words[1]]
 
-    print markov_dict
+#   print markov_dict
 
     return markov_dict
 
@@ -43,30 +41,21 @@ def make_text(chains,n):
     based off an original text."""
 
     # find a bigram to start off
-    #TODO is there a better way to do this than popping then reinserting
-
-    print chains
+    #TODO is there a better way to do this than popping then reinserting?
     
-#TODO  this is inelegant.  We should iterate through until we find a tuple in which 
-# first word is capitalized and use that as a start
+    ngram = choice(chains.keys())
+    value = chains.get(ngram)
 
-    ngram,value = chains.popitem()
-    chains[ngram] = value
-    # print ngram[0]
-    # print ngram[]
+
+#using a wordlist since we need to append; will convert back to a string later.
     wordlist = []
-    #we'll convert back to a string later
-
 
 #we need to split the tuple in the key, create a new key with the second from the old
-    
-
-    # go through n ngrams
+        # go through n ngrams
     # start with the initial ingram
     # append the output string with that ngram, and random value
     # set new key to second of the earlier key and the value from above
     # for nmgram, value in chains.iteritems():
-    #     print key, value, choice(value)
 
     for i in range(int(n)):
          choiceword = choice(value)
@@ -75,26 +64,24 @@ def make_text(chains,n):
             wordlist.append(ngram[1])
          
          wordlist.append(choiceword)
-         print i
-         print wordlist
+         # print i
+         # print wordlist
 
          new_key = (ngram[1],choiceword)
          ngram = new_key
          value = chains[ngram]  
-    
-        
-# To get sentences, we need to capitalize the word after a sentence ender 
+
 
     return " ".join(wordlist)
 
 def main():
     filename = argv[1]
-# Do we want to change case, remove punctuation?
-# How many ngrams do we want?
+
+# How many ngrams do we want to ?
     n = argv[2]
 
-#open filename
-#TODO clean up argument handling
+#TODO clean up argument handling, pick a nice default for n, check for file existence, etc.
+
     f = open(filename, "r")
     input_text = f.read()
     f.close()
